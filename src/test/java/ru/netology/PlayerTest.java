d837d281705d43ba0b8f413388736435f7009cbb
Mikhail Polovnikov <mishapolovnikov@gmail.com>
2022-12-14 21:00:16 +0500

    add test for Player class

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -36,6 +36,17 @@ public class PlayerTest {
         int actual = player.sumGenre(game.getGenre());
         assertEquals(expected, actual);
     }
+    @Test
+    public void shouldExpectionTest() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Game 1", "Genre1");
+
+        Player player = new Player("Player1");
+
+        assertThrows(RuntimeException.class, () -> {
+            player.play(game, 5);
+        });
+    }
 
     @Test
     public void mostPlayerByGenre() {
@@ -58,6 +69,41 @@ public class PlayerTest {
 
         String expected = "Game2";
         String actual = player.mostPlayerByGenre("Genre1");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shoudGameforGenreIfNotPlay() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Game1", "Genre1");
+        Game game1 = store.publishGame("Game2", "Genre2");
+        Game game2 = store.publishGame("Game3", "Genre3");
+
+        Player player = new Player("Player1");
+        player.installGame(game);
+        player.play(game, 1);
+        player.installGame(game1);
+        player.play(game1, 2);
+        player.installGame(game2);
+        player.play(game2, 3);
+
+
+        String expexted = null;
+        String actual = player.mostPlayerByGenre("Genre4");
+    }
+    @Test
+    public void shouldSumPlayTwoGameWhenPlayOneGame() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Game1", "Genre1");
+        Game game1 = store.publishGame("Game2", "Genre2");
+
+        Player player = new Player("Player1");
+        player.installGame(game);
+        player.play(game, 3);
+        player.installGame(game1);
+
+        int expected = 3;
+        int actual = player.sumGenre(game.getGenre());
+
         assertEquals(expected, actual);
     }
 }
